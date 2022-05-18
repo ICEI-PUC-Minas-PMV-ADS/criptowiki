8fb1237360ea7f6223175a90ca2d0b250b96038a
erickzz <erick.reis2@gmail.com>
2022-05-18 14:22:49 -0300

    Legenda busca

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -21,3 +21,14 @@ function busca(){
 });
  */
 
+function legenda(){
+    var caixa = document.querySelector('.tooltip')
+    var caixa2 = caixa.classList.toggle('tooltip2')
+    console.log('sim')
+}
+
+function legenda2(){
+    var caixa = document.querySelector('.tooltip2')
+    var caixa2 = caixa.classList.remove('tooltip2')
+    console.log('não')
+}
