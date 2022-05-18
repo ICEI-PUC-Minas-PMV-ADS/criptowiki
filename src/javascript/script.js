4db01441027c78ec28467f1cca1676c14d60ba2f
erickzz <erick.reis2@gmail.com>
2022-05-18 11:57:50 -0300

    Enter funcionando \o/

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -8,14 +8,16 @@ function busca(){
     var link = document.getElementById('link').href=`https://coinmarketcap.com/pt-br/currencies/${textoEspaco}/`
 }
 
-document.addEventListener("keypress",function(e){
+/* document.addEventListener("keypress",function(e){
     if (e.key === "Enter"){
 
-        var btn = document.querySelector("#botaobusca").click()
+        var btn = document.querySelector("#link")
+
+        btn.click()
         
         console.log("Enter")
 
     }
 });
-
+ */
 
