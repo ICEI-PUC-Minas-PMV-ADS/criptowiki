dbdf50d3341d916ddb30ab4dba431b588a1d6156
erickzz <erick.reis2@gmail.com>
2022-05-18 11:45:22 -0300

    Responsividade e protótipo da busca

---
+++ src/javascript/script.js
@@ -0,0 +1,21 @@
+
+// pesquisar.addEventListener('click', busca)
+
+function busca(){
+    var texto = document.querySelector('#caixabusca').value
+    var texto = String(texto).toLocaleLowerCase()
+    var textoEspaco = texto.replace(/ /g,'-')
+    var link = document.getElementById('link').href=`https://coinmarketcap.com/pt-br/currencies/${textoEspaco}/`
+}
+
+document.addEventListener("keypress",function(e){
+    if (e.key === "Enter"){
+
+        var btn = document.querySelector("#botaobusca").click()
+        
+        console.log("Enter")
+
+    }
+});
+
+
